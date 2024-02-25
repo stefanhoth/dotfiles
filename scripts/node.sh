731834ce96004f1f8209db6a3908e017fcddd512
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2024-02-25 19:33:06 +0100

    feat(node): Use fnm instead of nvm
    fnm is written in rust and much faster

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,7 +2,7 @@
 
 echo ""
 echo "#############################"
-echo "# Running global npm setup from npm.sh"
+echo "# Running global node setup from node.sh"
 echo "#############################"
 echo ""
 
@@ -10,36 +10,18 @@ ROOT_DIR=$1
 CONFIG_DIR="$ROOT_DIR/config"
 
 # *************************************
-# node
+# fnm
 #
 
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
-}
+# Check for fnm, install if not present
+fnm --version >/dev/null 2>&1 || /bin/bash -c "brew install fnm"
 
-LATEST_NVM_TAG=$(get_latest_release "nvm-sh/nvm")
+echo "installing node via fnm (lts + latest release)"
+fnm install --lts
+fnm install --latest
 
-echo "installing node via nvm ($LATEST_NVM_TAG)"
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_NVM_TAG/install.sh" | bash
-printf "installed nvm\n"
+NODE_CURRENT=$(fnm current)
+echo "Currently using node $NODE_CURRENT for now. Use 'fnm use VERSION' to change this. Use 'fnm list' to see all installed versions."
 
-# loading nvm for now
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-if [ -L "$NVM_DIR/default-packages" ]; then
-  echo "found nvm default-packages - removing..."
-  rm "$NVM_DIR/default-packages"
-fi
-
-echo "linking $NVM_DIR/default-packages"
-ln -s "$CONFIG_DIR/package-lists/nvm-default-packages.txt" "$NVM_DIR/default-packages"
-
-nvm install stable
-nvm alias default stable
-nvm use stable
-
-printf "updating npm\n"
-npm i -g npm
+echo "Installing global packages from $CONFIG_DIR/package-lists/npm-global-packages.txt"
+npm install --global $(tr '\n' ' ' < $CONFIG_DIR/package-lists/npm-global-packages.txt)
